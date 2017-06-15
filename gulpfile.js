42c8bdb758e0bc9c5a7e796e1b2e4a2932e7a693
Stefan Batocanin <batocko93@gmail.com>
2017-06-15 19:22:53 +0200

    Project setup and app init implemented

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
+
+gulp.task('default', ['watch'], function () {
+});
+
+gulp.task('styles', function() {
+  return gulp.src('assets/stylesheets/*.scss')
+    .pipe(sass({outputStyle: 'compressed', sourceComments: 'map'}, {errLogToConsole: true}))
+    .pipe(autoprefixer('last 4 version'))
+    .pipe(gulp.dest('assets/stylesheets'));
+});
+
+gulp.task('scripts', function() {
+  return gulp.src('assets/javascripts/*.js')
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('assets/javascripts/minified'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('assets/stylesheets/*.scss', ['styles']);
+    gulp.watch('assets/javascripts/*.js', ['scripts']);
+});
